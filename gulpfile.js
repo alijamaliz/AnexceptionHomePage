fcd5955e7492477591e20710f0e62a48508ba78e
Ali <jamaliali219@gmail.com>
2019-04-03 20:18:44 +0430

    Implement gulp-connect for debug env

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var connect = require('gulp-connect');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var runSequence = require('run-sequence');
@@ -8,10 +9,21 @@ var cleanCSS = require('gulp-clean-css');
 var sass = require('gulp-sass');
 var CacheBuster = require('gulp-cachebust');
 
+var config = require('./config.json');
+
 var cachebust = new CacheBuster();
+
+gulp.task('connect', function () {
+    connect.server({
+        port: 3000,
+        fallback: './index.html'
+    });
+});
+
 
 gulp.task('build-libraries-script', function () {
     return gulp.src([
+        './node_modules/particles.js/particles.js',
     ])
         .pipe(babel())
         .pipe(concat('Libs.js'))
@@ -40,7 +52,7 @@ gulp.task('build-scripts', function () {
     return gulp.src([
         './scripts/**/*.js'
     ])
-        .pipe(babel({ presets: ['env'] }))
+        // .pipe(babel({ presets: ['env'] }))
         .pipe(concat('Scripts.js'))
         .pipe(gulp.dest('./dist'))
         .pipe(rename('Scripts.min.js'))
@@ -64,6 +76,7 @@ gulp.task('build-styles', function () {
 gulp.task('build-html', [
     'build-styles',
     'build-scripts',
+    'build-libraries-script'
 ], function () {
     return gulp.src('index.html')
         .pipe(cachebust.references())
@@ -71,13 +84,17 @@ gulp.task('build-html', [
 });
 
 
+let sequence = [
+    'build-scripts',
+    'build-libraries-script',
+    'build-libraries-style',
+    'build-styles',
+    'build-html'
+];
+if (config.debug) {
+    sequence.push('connect');
+}
 
 gulp.task('default', function () {
-    runSequence([
-        'build-scripts',
-        'build-libraries-script',
-        'build-libraries-style',
-        'build-styles',
-        'build-html'
-    ]);
+    runSequence(sequence);
 });
